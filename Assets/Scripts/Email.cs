212f6e9ed99fe713f4dfea0e3f62bc4f09b64a87
yevhenkr <vasilenkoevgen88@gmail.com>
2020-05-19 19:02:04 +0300

    Send Email

---
+++ Assets/Scripts/Email.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Email : MonoBehaviour
+{
+     public void SendEmail()
+  {
+    string email = "tarasawrik@gmail.com";
+    string subject = MyEscapeURL("My Subject");
+    string body = MyEscapeURL("My Body\r\nFull of non-escaped chars");
+    Application.OpenURL("mailto:" + email + "?subject=" + subject + "&body=" + body);
+
+    string MyEscapeURL(string url)
+    {
+      return WWW.EscapeURL(url).Replace("+", "%20");
+    }
+
+  }
+}

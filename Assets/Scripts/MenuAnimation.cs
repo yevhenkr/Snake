90750594caeaa4f6aab0590d0f838a2e6d37e09f
yevhenkr <vasilenkoevgen88@gmail.com>
2020-07-23 23:14:47 +0300

    p

---
+++ Assets/Scripts/MenuAnimation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class MenuAnimation : MonoBehaviour
+{
+    [SerializeField] private Transform burgerPanel;
+    [SerializeField] private Transform target;
+    [SerializeField] private Transform rightEdgeScreeen;
+    [SerializeField] private float durationBurger
+        ;
+
+    // Update is called once per frame
+    void Start()
+    {
+       //burgerPanel.position = Vector3.Lerp(rightEdgeScreeen.position,burgerPanel.position, Time.deltaTime*2f);
+       burgerPanel.DOMove(target.position, durationBurger, false);
+    }
+}
